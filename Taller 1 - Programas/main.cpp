01b497772de5fe0aa37854eda198e94d601c82d1
alejoa172 <aaguilar@utp.edu.co>
2017-02-08 15:12:05 -0500

    Programas

---
+++ Taller 1 - Programas/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	//1 Multiplicacion con sumas 
+
+	
+	/*int x, y, producto; 
+	cout<< "ingrese numeros enteros a multiplicar : " <<endl;
+	cin>>x;
+	cin>>y;
+	
+	producto=0;
+	
+	for (int i=1; i<=y; i++)
+	{
+		producto=producto+x ;
+	
+	}
+	
+	cout<<"el producto de la multiplicacion es "<<producto << endl;*/
+	
+//3 Polinomio grado n 
+
+/*int n; 
+cout<<"ingrese el # de coeficientes" ;
+cin>>n;
+float a [n]; //arreglo de coe
+float x;
+
+for (int i=0; i<n; i++) {
+	
+	cout<<"coeficiente a ["<< i <<"] = ";
+	cin>> a [i];
+	}
+	cout<<"ingrese valor de x =";
+	cin>>x;
+	
+	//calcular valor numerico P(x)
+	float p=0.0;
+	
+	for (int i=0; i<n; i++) {
+	p=p+a[i] * pow (x, i);
+	
+	}
+cout<<"\n valor numerico p ("<<x<<") = " <<p<<endl;
+*/
+
+
+
+
+
+
+	//5 Division de dos enteros 
+/*	
+int dividendo,divisor,cociente=0,residuo; 
+cout<<"introduzca el dividendo\n"; 
+cin>>dividendo; 
+cout<<"introduzca el divisor\n"; 
+cin>>divisor; 
+
+while(dividendo>divisor){ 
+
+dividendo=dividendo-divisor; 
+cociente=cociente+1; 
+
+} 
+
+residuo=dividendo; 
+cout<<"el resultado de la division es "<<cociente<<" y el residuo es "<<residuo; */
+	
+	
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
