96af10db3c0d926b490841c700d52c32aac3af31
alejoa172 <aaguilar@utp.edu.co>
2017-02-08 22:39:29 -0500

    Update main.cpp

--- Taller 1 - Programas/main.cpp
+++ Taller 1 - Programas/main.cpp
@@ -48,7 +48,21 @@ for (int i=0; i<n; i++) {
 	}
 cout<<"\n valor numerico p ("<<x<<") = " <<p<<endl;
 */
+	
+//4 Factorial de un numero entero
 
+/*int fac=0; 
+printf("Ingresa numero para calcular el factorial "); 
+scanf("%d",&fac); 
+int temp=fac-1; 
+int r=fac; 
+while (temp>=1) 
+{ 
+r=r*temp; 
+temp--; 
+} 
+
+printf("El factorial de %d es: %d ", fac,r); */
 
 
 
